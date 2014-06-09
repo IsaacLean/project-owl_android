39dc0c78d985912af68b6f62b4672201f333c50a
Isaac Lean <isaaclean@creatibyte.com>
2014-06-09 06:14:15 -0700

    JSON can now be downloaded and read

--- src/com/isaaclean/projectowl/FinanceActivity.java
+++ src/com/isaaclean/projectowl/FinanceActivity.java
@@ -1,10 +1,26 @@
 package com.isaaclean.projectowl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -15,6 +31,7 @@ public class FinanceActivity extends Activity {
 	String[] itemArray = {"Currently no items"};
 	ArrayAdapter<String> itemAdapter;
 	Context context;
+	String feedUrl = "http://isaaclean.com/news_sources.json";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -62,9 +79,44 @@ public class FinanceActivity extends Activity {
 		
 		@Override
 		protected Void doInBackground(Void... params){
+			HttpClient client = new DefaultHttpClient();
+			HttpGet getRequest = new HttpGet(feedUrl);
+			
 			try{
-				Thread.sleep(3000);
-			}catch(InterruptedException e){
+				HttpResponse response = client.execute(getRequest);
+				StatusLine statusLine = response.getStatusLine();
+				int statusCode = statusLine.getStatusCode();
+				
+				if(statusCode != 200){
+					return null;
+				}
+				
+				InputStream jsonStream = response.getEntity().getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(jsonStream));
+				StringBuilder builder = new StringBuilder();
+				String line;
+				
+				while((line = reader.readLine()) != null){
+					builder.append(line);
+				}
+				
+				String jsonData = builder.toString();
+				
+				//Start reading JSON
+				//Log.i("ProjectOwl", jsonData);
+				JSONObject json = new JSONObject(jsonData);
+				JSONArray sites = json.getJSONArray("sites");
+				for(int i=0; i < sites.length(); ++i){
+					JSONObject item = sites.getJSONObject(i);
+					Log.d("ProjectOwl", item.toString());
+					String title = item.getString("title");
+					Log.d("ProjectOwl", title);
+				}
+			}catch(ClientProtocolException e){
+				e.printStackTrace();
+			}catch(IOException e){
+				e.printStackTrace();
+			}catch(JSONException e){
 				e.printStackTrace();
 			}
 			return null;
